3a15ca320290307eddd6e78108ee64260840bb9a
Nicole Kurre <nicole.a.kurre@gmail.com>
2022-09-01 21:51:20 -0700

    Adding final touches to Java +cleanup

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,66 @@
-// // Assignment Code: what I want to accomplish -
-//     // (done) When I select generate password, then a list of criteria display
-//     // (done) and a box for the length of the pasword is displayed
-//     // when no criteria is selected then red text appears asking them to check at least one box
-//     // If no length is provided then have a warning appear asking them to provide a length
-//     // If a whole number isn't entered then an altert pops up asking them to provide a whole number
-//     // If they enter less then 8, then an altert appears saying they need to enter a higher number
-//     // If they enter a number more than 128 then an alert appears telling them they need to enter a shorter length
-//     // Once at least one criteria is provided and a length is selected then a password automatically appears
-//     // when a new check box is selected then the password chnages
-//     // when the length is chnaged then the password length chnages
-
-// // Add event listener to generate button
-
+// global variable for generating the criteria list
 var generatecrit = document.getElementById('generatecrit');
 
+// event lisener assisgned to clicking on the "Generate password" button that defines CSS class to "show" which displays the criteria
 generatecrit.addEventListener("click", function(){
   document.getElementById('textInput').className = "show";
 });
 
 
-// calling all variables
+// calling all variables needed from HTML (HTML variables added to enable check boxes and length entry)
 var passwordresult = document.getElementById('password');
 var passwordlength = document.getElementById('length');
-var uppercaseDOM = document.getElementById('uppercase');
-var lowercaseDOM = document.getElementById('lowercase');
-var numbersDOM = document.getElementById('numbers');
-var symbolsDOM = document.getElementById('symbols');
+var uppercaseCheckbox = document.getElementById('uppercase');
+var lowercaseCheckbox = document.getElementById('lowercase');
+var numbersCheckbox = document.getElementById('numbers');
+var symbolsCheckbox = document.getElementById('symbols');
 var generatebtn = document.getElementById('generate');
 
 
 // Password Generating Function
 function genPassword () {
+//  if functions below specify the set of variables that need to be used for a combination of checkboxes and provides characters
   let chars = "null";
-  if (numbersDOM.checked == true && symbolsDOM.checked == false && uppercaseDOM.checked == false && lowercaseDOM.checked == false) chars = "0123456789";
-  if (numbersDOM.checked == true && symbolsDOM.checked == true && uppercaseDOM.checked == false && lowercaseDOM.checked == false) chars = "0123456789!@#$%^&*()";
-  if (numbersDOM.checked == true && symbolsDOM.checked == false && uppercaseDOM.checked == true && lowercaseDOM.checked == false) chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (numbersDOM.checked == true && symbolsDOM.checked == true && uppercaseDOM.checked == true && lowercaseDOM.checked == false) chars = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (numbersDOM.checked == true && symbolsDOM.checked == true && uppercaseDOM.checked == true && lowercaseDOM.checked == true) chars = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  if (numbersDOM.checked == false && symbolsDOM.checked == true && uppercaseDOM.checked == true && lowercaseDOM.checked == false) chars = "!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (numbersDOM.checked == false && symbolsDOM.checked == true && uppercaseDOM.checked == false && lowercaseDOM.checked == false) chars = "!@#$%^&*()";
-  if (numbersDOM.checked == false && symbolsDOM.checked == false && uppercaseDOM.checked == true&& lowercaseDOM.checked == false) chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (numbersDOM.checked == true && symbolsDOM.checked == false && uppercaseDOM.checked == false && lowercaseDOM.checked == true) chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-  if (numbersDOM.checked == false && symbolsDOM.checked == true && uppercaseDOM.checked == false && lowercaseDOM.checked == true) chars = "!@#$%^&*()abcdefghijklmnopqrstuvwxyz";
-  if (numbersDOM.checked == false && symbolsDOM.checked == false && uppercaseDOM.checked == true && lowercaseDOM.checked == true) chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  if (numbersDOM.checked == false && symbolsDOM.checked == false && uppercaseDOM.checked == false && lowercaseDOM.checked == true) chars = "abcdefghijklmnopqrstuvwxyz";
-  if (numbersDOM.checked == true && symbolsDOM.checked == true && uppercaseDOM.checked == false && lowercaseDOM.checked == true) chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-  if (numbersDOM.checked == true && symbolsDOM.checked == false && uppercaseDOM.checked == true && lowercaseDOM.checked == true) chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (numbersDOM.checked == false && symbolsDOM.checked == true && uppercaseDOM.checked == false && lowercaseDOM.checked == true) chars = "!@#$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // if (symbolsDOM.checked) chars = "!@#$%^&*()";
-  // if (uppercaseDOM.checked) chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = passwordlength.value;
-  var password = "";
- for (var i = 0; i <= passwordLength; i++) {
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false) chars = "0123456789";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false) chars = "0123456789!@#$%^&*()";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == false) chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == false) chars = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == true) chars = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == false) chars = "!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false) chars = "!@#$%^&*()";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == true&& lowercaseCheckbox.checked == false) chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == true) chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == true) chars = "!@#$%^&*()abcdefghijklmnopqrstuvwxyz";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == true) chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == true) chars = "abcdefghijklmnopqrstuvwxyz";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == true) chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+  if (numbersCheckbox.checked == true && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == true) chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == true && uppercaseCheckbox.checked == true && lowercaseCheckbox.checked == true) chars = "!@#$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if ((numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false)) chars = "";
+  
+  // if functions below set parameters for password generation + support no password definition when no boxes are checked or length is provided
+  let passwordLength = passwordlength.value;
+  if ((passwordLength < 8) || (passwordLength > 128)) chars = "";
+  let password = "";
+  if ((numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false) || (passwordLength < 8)|| (passwordLength > 128)) password = "";
+  
+  // Password generating loop function based off of defined characters provided by ckeckboxes and if statements above; beginning initalizes loop (password input is subtracted by 1 to account for the 0 index); math floor and mathrandom provide a random string that is parsed out in the sub-string to generate a password with the accurate criteria used and length selected
+  for (var i = 0; i <= (passwordLength - 1); i++) {
    var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
+   password += chars.substring(randomNumber, randomNumber + 1);
   }
+
+  //writes the password to the password box (console log to check for the value)
   document.getElementById("password").value = password;
- }
+  console.log(password)
+  console.log(passwordLength)
+  
+  //defines alert messages when criteria boxes are not selected or length falls out of the range
+  let charerror = "";
+  if (numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false) charerror = "Error: Select at least on character criteria";
+  if (passwordLength < 8) charerror = "Error: Select at length of at least 8";
+  if (passwordLength > 128) charerror = "Error: Select at length less than 129";
+  if ((numbersCheckbox.checked == false && symbolsCheckbox.checked == false && uppercaseCheckbox.checked == false && lowercaseCheckbox.checked == false) || (passwordLength < 8) || (passwordLength > 128)) {
+    alert(charerror)
+  }
+}
